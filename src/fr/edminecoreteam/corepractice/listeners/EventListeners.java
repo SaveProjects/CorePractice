1749f4ae65f3d6c0cd8a86a3f9df8b6312df6e54
EdmineOfficialGitHub <edminerepository@gmail.com>
2023-07-04 23:18:07 +0200

    add event

--- src/fr/edminecoreteam/corepractice/listeners/EventListeners.java
+++ src/fr/edminecoreteam/corepractice/listeners/EventListeners.java
@@ -1,6 +1,8 @@
 package fr.edminecoreteam.corepractice.listeners;
 
 import fr.edminecoreteam.corepractice.Core;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,6 +23,17 @@ public class EventListeners implements Listener
             if (core.getInLobby().contains(p) || core.getInEditor().contains(p))
             {
                 e.setCancelled(true);
+                if (e.getCause().equals(EntityDamageEvent.DamageCause.VOID))
+                {
+                    Location lobbySpawn = new Location(Bukkit.getWorld(core.getConfig().getString("Lobby.world")),
+                            (float) core.getConfig().getDouble("Lobby.x")
+                            , (float) core.getConfig().getDouble("Lobby.y")
+                            , (float) core.getConfig().getDouble("Lobby.z")
+                            , (float) core.getConfig().getDouble("Lobby.t")
+                            , (float) core.getConfig().getDouble("Lobby.b"));
+                    p.teleport(lobbySpawn);
+                    ((Player) p).sendTitle("§c§lHop Hop Hop !", "§7Tu ne peux pas quitter la cité des dieux...");
+                }
             }
         }
     }
