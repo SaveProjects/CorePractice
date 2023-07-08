347a9a00e706b067af2e37e94223dbe715452978
EdmineOfficialGitHub <edminerepository@gmail.com>
2023-07-08 04:45:50 +0200

    modification

--- src/fr/edminecoreteam/corepractice/blocs/BlocsListener.java
+++ src/fr/edminecoreteam/corepractice/blocs/BlocsListener.java
@@ -2,35 +2,48 @@ package fr.edminecoreteam.corepractice.blocs;
 
 import fr.edminecoreteam.corepractice.Core;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlocsListener implements Listener
 {
-    private Core core = Core.getInstance();
+    private final Core core = Core.getInstance();
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        if (core.getBlocsToWorld().isBlockProtected(event.getBlock())) {
-            event.setCancelled(true);
-        }
-    }
 
-    @EventHandler
-    public void onEntityExplode(EntityExplodeEvent event) {
-        for (Block block : event.blockList()) {
-            if (core.getBlocsToWorld().isBlockProtected(block)) {
+        Player p = event.getPlayer();
+
+        if (core.getInDuel().contains(p))
+        {
+            if (!core.getBlocsToWorld().getPlayerBlocks(p).contains(event.getBlock()))
+            {
                 event.setCancelled(true);
-                return;
             }
         }
     }
 
     @EventHandler
-    public void onWorldUnload(WorldUnloadEvent event) {
-        core.getBlocsToWorld().removeBlocksFromWorld(event.getWorld().getName());
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+
+        // Vérifier si le joueur est déjà dans la map
+        if (core.getBlocsToWorld().getPlayerBlocks().containsKey(player)) {
+            core.getBlocsToWorld().getPlayerBlocks().get(player).add(block);
+        } else {
+            List<Block> blocks = new ArrayList<>();
+            blocks.add(block);
+            core.getBlocsToWorld().getPlayerBlocks().put(player, blocks);
+        }
     }
+
 }
--- src/fr/edminecoreteam/corepractice/blocs/BlocsToWorld.java
+++ src/fr/edminecoreteam/corepractice/blocs/BlocsToWorld.java
@@ -1,68 +1,36 @@
 package fr.edminecoreteam.corepractice.blocs;
 
-import net.minecraft.server.v1_8_R3.ChunkProviderServer;
-import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
-import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BlocsToWorld
 {
-    private final List<Block> protectedBlocks;
+    private Map<Player, List<Block>> playerBlocks;
 
     public BlocsToWorld() {
-        protectedBlocks = new ArrayList<>();
+        playerBlocks = new HashMap<>();
     }
 
-    public void addBlocksToWorld(String worldName) {
-        World world = Bukkit.getWorld(worldName);
-        if (world != null) {
-            int chunkCount = world.getLoadedChunks().length;
-            for (int i = 0; i < chunkCount; i++) {
-                Chunk chunk = world.getLoadedChunks()[i];
-                for (int x = 0; x < 16; x++) {
-                    for (int y = 0; y < world.getMaxHeight(); y++) {
-                        for (int z = 0; z < 16; z++) {
-                            Block block = chunk.getBlock(x, y, z);
-                            protectedBlocks.add(block);
-                        }
-                    }
-                }
+    public boolean isBlockPlaced(Block block) {
+        for (List<Block> blocks : playerBlocks.values()) {
+            if (blocks.contains(block)) {
+                return true;
             }
-        } else {
-            Bukkit.getLogger().warning("Le monde spécifié n'existe pas !");
         }
+        return false;
     }
 
-    public void removeBlocksFromWorld(String worldName) {
-        World world = Bukkit.getWorld(worldName);
-        if (world != null) {
-            protectedBlocks.removeIf(block -> block.getWorld().equals(world));
-        } else {
-            Bukkit.getLogger().warning("Le monde spécifié n'existe pas !");
-        }
+    public List<Block> getPlayerBlocks(Player player) {
+        return playerBlocks.getOrDefault(player, new ArrayList<>());
     }
 
-    public boolean isBlockProtected(Block block) {
-        return protectedBlocks.contains(block);
-    }
-
-    public void loadChunks(String worldName) {
-        World world = Bukkit.getWorld(worldName);
-        if (world != null) {
-            int chunkCount = 0;
-            for (org.bukkit.Chunk chunk : world.getLoadedChunks()) {
-                if (!chunk.isLoaded()) {
-                    world.loadChunk(chunk.getX(), chunk.getZ());
-                    chunkCount++;
-                }
-            }
-            Bukkit.getLogger().info("Chunks chargés pour le monde " + worldName + " : " + chunkCount);
-        } else {
-            Bukkit.getLogger().warning("Le monde spécifié n'existe pas !");
-        }
+    public Map<Player, List<Block>> getPlayerBlocks()
+    {
+        return playerBlocks;
     }
 }
